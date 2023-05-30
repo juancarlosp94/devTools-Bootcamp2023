80b1269aea8dea08b0c61d97672c9a8336620fcc
JUAN_PERDIGON <juanc9410@gmail.com>
2023-05-30 16:07:53 -0500

    Reestructuración de carpetas y archivos

--- README.txt.txt
+++ README.txt.txt
@@ -1,1 +1,3 @@
 Hola este archivo tiene el proposito de modificar la rama main para el ejercicio #1 del Bootcamp DevTools 2023
+
+
---
+++ ejerciciosOpenBootcamp/CursoJavaScript/primer-proyecto-node/index.js
@@ -0,0 +1,11 @@
+// Este es el mensaje de bienvenida
+
+console.log("Esta es la puerta de entrada al proyecto");
+
+/* asdfasdfasdfawdf
+asdfasdfasdfasdfasd
+asdfasdfasdfasdfasd */
+
+// Este va a ser el mensaje que se envía para despedir
+
+console.log("Adiós");
